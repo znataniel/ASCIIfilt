d00e3055f0d59e1fd0607d9e0fa7dd6b65451e4f
znataniel <znataniel@protonmail.com>
2024-07-09 15:49:07 -0300

    Improve grayscale transformation function. Cleanup. Vertical side-by-side

--- src/main.py
+++ src/main.py
@@ -2,27 +2,24 @@ from PIL import Image
 import numpy as np
 
 
+# To convert RGB to 8 bit gamma:
+# gamma = 0.2126*red + 0.7152*green + 0.0722*blue
 def rgb_to_grayscale(pixels):
-    gs_pixels = []
-    for col in pixels:
-        new_col = []
-        for t in col:
-            # just averaging RGB and converting it to 8-bit
-            gs_p = (t[0] + t[1] + t[2]) % 255
-            new_col.append(gs_p)
-        gs_pixels.append(new_col)
-    return gs_pixels
+    def pixel_rgb_to_gray(t):
+        return 0.2126 * t[0] + 0.7152 * t[1] + 0.0722 * t[2]
+
+    return [[pixel_rgb_to_gray(t) for t in col] for col in pixels]
 
 
 def side_by_side(im_1, im_2, gap=20):
-    sbs = Image.new("RGB", (im_1.size[0] + im_2.size[0] + gap, im_1.size[0]))
+    sbs = Image.new("RGB", (im_1.size[1], im_1.size[1] + im_2.size[1] + gap))
     sbs.paste(im_1)
-    sbs.paste(im_2, (im_1.size[0] + gap, 0))
+    sbs.paste(im_2, (0, im_1.size[1] + gap))
     return sbs
 
 
 def main():
-    im = Image.open("assets/funny_dog.jpg")
+    im = Image.open("assets/in.jpg")
     size = im.size
 
     # each sublist is a column
@@ -30,9 +27,9 @@ def main():
 
     gs_pixels = rgb_to_grayscale(rgb_pixels)
 
-    gs = Image.fromarray(np.array(gs_pixels, dtype=np.uint8), "L")
+    gs = Image.fromarray(np.array(gs_pixels, dtype=np.uint8))
 
-    side_by_side(im, gs).show()
+    side_by_side(im, gs).save("assets/out.jpg")
 
 
 if __name__ == "__main__":
